f1608fe30990eac9e813bc80e427371ef9ebd570
narv1 <narv@onders.org>
2015-10-13 16:49:46 +0200

    Ausgabe sowie RegValue verfeinert

--- RegSim.c
+++ RegSim.c
@@ -5,7 +5,7 @@
 /* register initialisieren */
 /***************************/
 
-struct byte8{
+struct byte{
   unsigned char b0:1, b1:1, b2:1, b3:1, b4:1, b5:1, b6:1, b7:1;
 };
 
@@ -14,24 +14,49 @@ struct byte8{
 /**********************/ 
 
 typedef union{
-  struct byte8 b;
+  struct byte bit;
   unsigned char byte;     // to show the programm: this is a bunch of bits.
-}byte8;
+}reg_byte;
 
-byte8 in1, in2, out;
+void RegValues(reg_byte *ptr){
 
-void RegValues(){
-  printf("Binaer\tInteger\tHex");
-  printf("cool");
+  printf("Binaere\t\tInteger\t\tHex\n");
+  
+  printf("%d%d%d%d%d%d%d%d\t", ptr->bit.b7,  ptr->bit.b6, ptr->bit.b5, ptr->bit.b4,
+	 ptr->bit.b3, ptr->bit.b2, ptr->bit.b1, ptr->bit.b0);
+
+  printf("%d\t\t0x%x\n\n", ptr->byte, ptr->byte);
+
 }
+/*
+int ChangeValue(struct byte b){
+  //b.b1 = 1;
+  //b.b5 = 1;
+  bit.byte = 0xf0;
+  return 0;
+  }*/
 
 int main (void){
 
-  byte8 bit;
-  
+  reg_byte bit;
+  bit.byte = 0x00;
+  //struct byte *ptr,p;
+  //ptr = &p;
+   
   printf("Willkommen.\nDieses Programm Simuliert ein Register, indem man jedes einzelne bit ansprechen kann!\n\n");
-  printf("%x",bit);
-  RegValues();
+  //printf("%d",(*ptr).b0);
+  RegValues(&bit);
+
+  // struct byte byte2;
+  //ChangeValue(bit);
+  bit.byte = 0x12;
+  RegValues(&bit);
+
+  bit.byte = ~bit.byte;
+  RegValues(&bit);
+  
+  bit.byte = bit.byte << 1;
+  RegValues(&bit);
 
   return 0;
   
