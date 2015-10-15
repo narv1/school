a51488c9030208344cfcd28cd49a2a5565c30999
narv1 <narv@onders.org>
2015-10-15 20:30:10 +0200

    Erklärung der Bitverarbeitung

--- RegSim.c
+++ RegSim.c
@@ -22,36 +22,24 @@ typedef union{
 void RegValues(reg_byte *ptr){
 
   printf("Binaere\t\tInteger\t\tHex\n");
-  
   printf("%d%d%d%d%d%d%d%d\t", ptr->bit.b7,  ptr->bit.b6, ptr->bit.b5, ptr->bit.b4,
 	 ptr->bit.b3, ptr->bit.b2, ptr->bit.b1, ptr->bit.b0);
-
   printf("%d\t\t0x%x\n\n", ptr->byte, ptr->byte);
 }
-/*
-int ChangeValue(struct byte b){
-  //b.b1 = 1;
-  //b.b5 = 1;
-  bit.byte = 0xf0;
-  return 0;
-  }*/
 
 int main (void){
 
   reg_byte bit;
   bit.byte = 0x00;
-  //struct byte *ptr,p;
-  //ptr = &p;
    
   printf("Willkommen.\nDieses Programm Simuliert ein Register, indem man jedes einzelne bit ansprechen kann!\n\n");
-  //printf("%d",(*ptr).b0);
   RegValues(&bit);
 
   // struct byte byte2;
   //ChangeValue(bit);
-  bit.byte = 0x22;
-  //bit.bit.b1 = 1;
-  //bit.bit.b5 = 1;
+  bit.byte = 0x22;        //     <--  1.Moeglichkeit
+  //bit.bit.b1 = 1;              <--  2.Moeglichkeit                   Es gibt 2 Moeglichkeiten einzelne Bits anzusprechen entweder man spricht das selbst erstellt Byte an od. 
+  //Bit.bit.b5 = 1;                                                    man spricht die bits einzeln an. Sow wie links vom diesen Text dargestellt.
   RegValues(&bit);
 
   bit.byte = ~bit.byte;
